a82922dde8a327c1574c4b4e59e650896482c8fd
S. Hahner <44293258+sahahner@users.noreply.github.com>
2023-12-14 17:15:46 +0100

    move data

--- 00_get_data.sh
+++ 00_get_data.sh
@@ -11,21 +11,21 @@ mkdir original_meshes
 if [[ "$dataset" == "car_TRUCK" ]]
 then
     dataset_spec=car_TRUCK_r4_2204
-    wget https://owncloud.scai.fraunhofer.de/index.php/s/JjC4q2ctL7Xoi2M/download/car_TRUCK_r4_2204.tar.gz
+    wget https://owncloud.scai.fraunhofer.de/index.php/s/kBBSrZFX8kpEbdn/download/car_TRUCK_r4_2204.tar.gz
     cd original_meshes
-    wget https://owncloud.scai.fraunhofer.de/index.php/s/8QCbtckZ4LQdCaF/download/car_TRUCK.tar.gz
+    wget https://owncloud.scai.fraunhofer.de/index.php/s/aQep26pRwnamDyf/download/car_TRUCK.tar.gz
 elif [[ "$dataset" == "car_YARIS" ]]
 then 
     dataset_spec=car_YARIS_r4_2204
-    wget https://owncloud.scai.fraunhofer.de/index.php/s/WYnZZ48XxCHPCty/download/car_YARIS_r4_2204.tar.gz
+    wget https://owncloud.scai.fraunhofer.de/index.php/s/tx3c3pm4GraBP49/download/car_YARIS_r4_2204.tar.gz
     cd original_meshes
-    wget https://owncloud.scai.fraunhofer.de/index.php/s/PbQDQDjXdrPMTnL/download/car_YARIS.tar.gz
+    wget https://owncloud.scai.fraunhofer.de/index.php/s/gzsiGYHYkaGTW4K/download/car_YARIS.tar.gz
 elif [[ "$dataset" == "FAUST" ]]
 then 
     dataset_spec=FAUST_r4_2203
-    wget https://owncloud.scai.fraunhofer.de/index.php/s/AP69eGwB7zrifHP/download/FAUST_r4_2203.tar.gz
+    wget https://owncloud.scai.fraunhofer.de/index.php/s/FzLg4st7Jr8BNeZ/download/FAUST_r4_2203.tar.gz
     cd original_meshes
-    wget https://owncloud.scai.fraunhofer.de/index.php/s/4DCADnJAa2r7dMP/download/FAUST.tar.gz
+    wget https://owncloud.scai.fraunhofer.de/index.php/s/t7paqc2SmAsPKFq/download/FAUST.tar.gz
 else
     echo "Not a valid dataset."
 fi
